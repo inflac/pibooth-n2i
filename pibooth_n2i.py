3027f93850607b5cb03031d9f18356254f024800
inflac <inflac@inflacsan.de>
2024-09-01 12:50:57 +0200

    fix logic error in config check && add upload request

--- pibooth_n2i.py
+++ pibooth_n2i.py
@@ -1,7 +1,6 @@
 """Plugin to upload pictures on a Nextride2-infobeamer instance.""" 
 
 import os
-import asyncio
 import requests as re
 
 import pibooth
@@ -11,22 +10,27 @@ __version__ = "0.0.1"
 
 SECTION = "N2i"
 
-async def upload(file, n2i_url:str, n2i_token:str):
+def upload(file:str, n2i_url:str, n2i_token:str):
+    headers = {'token': n2i_token}
+    file_param = {'file': open(file, 'rb')}
+
+    resp = re.post(n2i_url, files=file_param, headers=headers)
+    if resp.status_code != 200:
+        LOGGER.info("[N2i] Upload failed")
     print(f"N2i URL: {n2i_url}")
     print(f"N2i token: {n2i_token}")
-    print("Image: ", end="\r")
-    print(file)
+    print(f"Image: {file}")
 
-@pibooth.hookimpl 
+@pibooth.hookimpl
 def pibooth_startup(app, cfg):
     cfg.add_option(SECTION, "n2i_url", "", "Pibooth Endpoint URL")
     cfg.add_option(SECTION, "n2i_token", "", "Pibooth Auth Token")
 
 @pibooth.hookimpl
-async def state_processing_exit(app, cfg):
+def state_processing_exit(app, cfg):
     n2i_url = cfg.get(SECTION, "n2i_url")
     n2i_token = cfg.get(SECTION, "n2i_token") 
     
-    if n2i_url != "" and n2i_token != "":
-        LOGGER.error("No url or Token specified in config!")
-    await upload(open(app.previous_picture_file, 'rb'), n2i_url, n2i_token)
+    if n2i_url == "" and n2i_token == "":
+        LOGGER.error("[N2i] No url or Token specified in config!")
+    upload(app.previous_picture_file, n2i_url, n2i_token)
